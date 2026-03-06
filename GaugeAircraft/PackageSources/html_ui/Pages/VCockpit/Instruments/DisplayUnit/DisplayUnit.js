243337614c8426897282ed0972b7c90b40c7a7f8
alex853 <alexxxey.kornev@gmail.com>
2026-03-06 18:35:42 +0200

    display unit - engine sector gauges appeared!

--- GaugeAircraft/PackageSources/html_ui/Pages/VCockpit/Instruments/DisplayUnit/DisplayUnit.js
+++ GaugeAircraft/PackageSources/html_ui/Pages/VCockpit/Instruments/DisplayUnit/DisplayUnit.js
@@ -16,6 +16,17 @@ class DisplayUnit extends BaseInstrument {
 
         console.log("connected callback");
 
+        const primaryEnginePanelSvg = this.querySelector("#primary-engine-panel-svg");
+
+        this.leftEngineEprGauge = new EngineGauge(primaryEnginePanelSvg, 74, 80, "epr");
+        this.rightEngineEprGauge = new EngineGauge(primaryEnginePanelSvg, 317, 80, "epr");
+
+        this.leftEngineTgtGauge = new EngineGauge(primaryEnginePanelSvg, 74, 190, "tgt");
+        this.rightEngineTgtGauge = new EngineGauge(primaryEnginePanelSvg, 317, 190, "tgt");
+
+        this.leftEngineLpGauge = new EngineGauge(primaryEnginePanelSvg, 74, 302, "lp");
+        this.rightEngineLpGauge = new EngineGauge(primaryEnginePanelSvg, 317, 302, "lp");
+
         this.startUpdateStateCycle();
     }
 
@@ -36,6 +47,7 @@ class DisplayUnit extends BaseInstrument {
     }
 
     startUpdateStateCycle() {
+        let angle = 0;
         this._simUpdateInterval = setInterval(() => {
             // const page = Pages[this.currentPageIndex];
             // if (page.updateState)
@@ -44,8 +56,131 @@ class DisplayUnit extends BaseInstrument {
             // if (page.updateUI) {
             //     page.updateUI(this);
             // }
+
+            angle += 1;
+            if (angle >= 225) {
+                angle = 0;
+            }
+            this.leftEngineTgtGauge.setNeedleAngle(angle)
         }, 100);
     }
 }
 
 registerInstrument("display-unit-element", DisplayUnit);
+
+class EngineGauge {
+    constructor(svg, cx, cy, type) {
+        this.svg = svg;
+
+        this.cx = cx;
+        this.cy = cy;
+
+        const r = 52;
+        const arcLength = 225;
+        const arcWidth = 3;
+
+        const backgroundColor = "black";
+        const whiteColor = "#ddd";
+        const yellowArcColor = "#e0c000";
+        const redArcColor = "#d33";
+
+        this.createSector(cx, cy, r, 0, arcLength, backgroundColor);
+
+        if (type === "epr") {
+            this.createArc(cx, cy, r, 0, arcLength, whiteColor, arcWidth);
+        } else {
+            const yellowStart = type === "tgt" ? 150 : 185;
+            const redStart = type === "tgt" ? 190 : 205;
+
+            this.createArc(cx, cy, r, 0, yellowStart, whiteColor, arcWidth);
+            this.createArc(cx, cy, r, yellowStart, redStart, yellowArcColor, arcWidth);
+            this.createArc(cx, cy, r, redStart, arcLength, redArcColor, arcWidth);
+        }
+
+        this.needle = this.createNeedle(cx, cy, r-arcWidth/2, whiteColor);
+    }
+
+    createSector(cx, cy, radius, startDeg, endDeg, color) {
+        const startRad = startDeg * Math.PI / 180;
+        const endRad = endDeg * Math.PI / 180;
+
+        const x1 = cx + radius * Math.cos(startRad);
+        const y1 = cy + radius * Math.sin(startRad);
+
+        const x2 = cx + radius * Math.cos(endRad);
+        const y2 = cy + radius * Math.sin(endRad);
+
+        const largeArc = (endDeg - startDeg) > 180 ? 1 : 0;
+        const sweep = 1;
+
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+        const d = `M ${cx} ${cy}
+                          L ${x1} ${y1}
+                          A ${radius} ${radius} 0 ${largeArc} ${sweep} ${x2} ${y2}
+                          Z`;
+
+        path.setAttribute("d", d);
+        path.setAttribute("fill", color);
+        path.setAttribute("opacity", "0.5");
+
+        this.svg.appendChild(path);
+
+        return path;
+    }
+
+    createArc(cx, cy, r, startDeg, endDeg, color, width) {
+        const toRad = d => d * Math.PI / 180;
+
+        const x1 = cx + r * Math.cos(toRad(startDeg));
+        const y1 = cy + r * Math.sin(toRad(startDeg));
+
+        const x2 = cx + r * Math.cos(toRad(endDeg));
+        const y2 = cy + r * Math.sin(toRad(endDeg));
+
+        const largeArc = (endDeg - startDeg) > 180 ? 1 : 0;
+
+        const path = document.createElementNS("http://www.w3.org/2000/svg","path");
+
+        const d = `M ${x1} ${y1}
+                          A ${r} ${r} 0 ${largeArc} 1 ${x2} ${y2}`;
+
+        path.setAttribute("d", d);
+        path.setAttribute("fill","none");
+        path.setAttribute("stroke", color);
+        path.setAttribute("stroke-width", width);
+
+        this.svg.appendChild(path);
+
+        return path;
+    }
+
+    createNeedle(cx, cy, r, color) {
+        const needle = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+        const needleBaseRadius = 5;
+        const needleEndWidth = 0.5;
+
+        const d = `M 0 -${needleBaseRadius}
+                          A ${needleBaseRadius} ${needleBaseRadius} 0 0 0 0 ${needleBaseRadius}
+                          L ${r} ${needleEndWidth}
+                          L ${r} -${needleEndWidth}
+                          Z`;
+
+        needle.setAttribute("d", d);
+        needle.setAttribute("fill", color);
+
+        needle.setAttribute("transform", `translate(${cx},${cy}) rotate(0)`);
+
+        this.svg.appendChild(needle);
+
+        return needle;
+    }
+
+    setNeedleAngle(angleDeg) {
+        this.needle.setAttribute(
+            "transform",
+            `translate(${this.cx},${this.cy}) rotate(${angleDeg})`
+        );
+    }
+}
